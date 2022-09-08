6a7848bed75cd48245d7ff7e5074353da3d9075a
gmssameer <gmssameer@gmail.com>
2022-09-08 11:57:01 +0530

    three js portfolio

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+// 1. import `NextUIProvider` component
+import { NextUIProvider } from '@nextui-org/react'
 import '../styles/globals.css'
+import Layout from '../components/layouts/Main'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, router }) {
+  return (
+    <NextUIProvider>
+      <Layout router={router}>
+        <Component {...pageProps} key={router.route} />
+      </Layout>
+    </NextUIProvider>
+  )
 }
 
 export default MyApp
